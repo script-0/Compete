6158ec4ff354a6ccf9089e92ee404039ef2284ef
isaac <ndemabekolleemmanuelisaac@gmail.com>
2021-06-25 21:10:35 +0100

    Adding navigation

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
+  links = [
+    {
+      name : 'About',
+      component : 'About',
+      index : 1
+    },
+    {
+      name : 'Services',
+      component : 'Services',
+      index : 2
+    },
+    {
+      name : 'CyberGames',
+      component : 'CyberGames',
+      index : 3
+    }
+  ];
+  activeLink = 1;
 
   ngOnInit(): void {
   }
 
+  loadPage(index: number):void{
+    this.activeLink = index;
+  }
 }
