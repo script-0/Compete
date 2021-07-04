012438ba474b3b4a2cbdb64794c25d716b3f3fb0
isaac <ndemabekolleemmanuelisaac@gmail.com>
2021-07-04 21:53:42 +0100

    Resolve some bugs on app--routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { QuizComponent } from './quiz/quiz.component';
-import { RegisterComponent } from './register/register.component';
-import { ResultComponent } from './result/result.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { AuthGuard } from './auth/auth.guard';
-import { MainComponent } from './main/main.component';
-import { GamesComponent } from './games/games.component';
-import { AccountPopupComponent } from './account-popup/account-popup.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ResultComponent } from './components/result/result.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { AuthGuard } from './services/auth/auth.guard';
+import { MainComponent } from './components/main/main.component';
+import { GamesComponent } from './components/games/games.component';
+import { AccountPopupComponent } from './components/account-popup/account-popup.component';
 
 const routes: Routes = [
   { path:'games',component:GamesComponent},
